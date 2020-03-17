0f1f3ba52281525a4beb799d152699d06d563227
massimo.fornasier <massimo.fornasier@pixartprinting.com>
2020-03-17 19:26:38 +0100

    working but collision, score and life to fix

--- ninja.lua
+++ ninja.lua
@@ -10,12 +10,13 @@
     Game = {
         state = 0,
         -- at game start, generate enemy every 5 seconds
-        enemyGenerationRate=60*10,
+        enemyGenerationRate=60*5,
         timer=0
     }
 
     Player={
         x=96,
+        width=16,
         y=64,
         vx=0,
         vy=0,
@@ -27,7 +28,8 @@
         timer=0,
         moving=false,
         offset=0,
-        SpriteIndex=0
+        SpriteIndex=0,
+        score=0
     }
 
     EnemyList={}
@@ -70,6 +72,9 @@
     end
 
     function ManagePlayerMove()
+        if Player.life<=0 then
+            Game.state=2
+        end
         Player.moving = false
         Player.vx = 0
         if btn(2) and Player.offset==0 then 
@@ -101,13 +106,13 @@
         --]]
 
     function GenerateNewEnemy()
-        trace("generate new enemy")
+        --trace("generate new enemy")
         local e = {
-            x=0,
-            y=64,
+            x=-10,
+            y=75,
             vx=0,
             vy=0,
-            life=5,
+            life=1,
             attack=0,
             direction=0,
             onGround=true,
@@ -117,11 +122,24 @@
             offset=0,
             SpriteIndex=0
         }
+        local rnd = math.random(10)
+        if rnd%2==0 then
+            e.x=250
+            e.direction=0
+        end
         table.insert(EnemyList,e)
         EnemyListLength=EnemyListLength+1
     end
 
     function ManageEnemy()
+        for ei=0,EnemyListLength,1 do
+            if EnemyList[ei] ~= nil then
+                if EnemyList[ei].died and EnemyList[ei].timer%120==0 then
+                    table.remove(EnemyList,ei)
+                    break
+                end
+            end
+        end
         if Game.timer % Game.enemyGenerationRate == 0 then
             GenerateNewEnemy()
         end
@@ -130,30 +148,36 @@
     function ManageEnemyMove()
         for ei=0,EnemyListLength,1 do
             if EnemyList[ei] ~= nil then
+                if EnemyList[ei].died then
+                    EnemyList[ei].offset=10
+                    break
+                end
                 local e = EnemyList[ei]
                 if e.timer%4==0 then
                     if e.x<Player.x and e.attack==0 then    
+                        trace("fwd")
                         e.direction=1
                     end                
-                    if e.x>=Player.x and e.attack==0 then
+                    if e.x>=(Player.x) and e.attack==0 then
+                        trace("back")
                         e.direction=0
                     end
-
                     e.vy = 0
-                    if e.y <= Player.y  then
+                    if e.y <= 75  then
                         e.vy = 4
                     end
                     if e.attack>0 then
                         e.vy = -4
+                    end
+                    if e.attack > 0 then
                         e.vx = 4
-                        
                     end
-                    if e.attack <= 0 then
-                        e.vx = 1
+                    if e.attack==0 then
+                        e.vx=1
                     end
-                    trace(string.format("%s",e.vx))
+                    --trace(string.format("%s",e.vx))
                     e.y = e.y + e.vy
-                    if e.direction==false then
+                    if e.direction==0 then
                         e.x = e.x - e.vx
                     else
                         e.x = e.x + e.vx
@@ -167,19 +191,32 @@
         for ei=0,EnemyListLength,1 do
             if EnemyList[ei] ~= nil then
                 local e = EnemyList[ei]
-                e.offset=0
+                if e.died==false then
+                    e.offset=0
+                end
                 if e.attack>=30 then
-                    e.attack=-60
+                    e.attack=-30
                 end
                 if e.attack<0 then
                     e.attack=e.attack+1
                 end
-                if math.abs(e.x-Player.x)<15 and e.attack==0 then
+                trace(string.format("e:%s p:%s e.x-Player.x:%s atk:%s",e.x,Player.x,e.x-Player.x,e.attack))
+                if (math.abs(Player.x-e.x)<Player.width) and e.attack==0 then
                     e.attack=1
                     e.timer=0
+                    if Player.attack>e.attack then
+                        e.life = e.life-1
+                        Player.score = Player.score+1
+                    else
+                        Player.life = Player.life-1
+                    end
+                    if e.life<=0 then
+                        e.died=true
+                        e.timer=0
+                        e.attack=0
+                    end
                 end
                 if e.attack > 0 then
-                    --trace(string.format("enemy attack: %s",e.attack))
                     e.attack = e.attack+1
                     e.offset = 4
                 end
@@ -191,7 +228,11 @@
         for ei=0,EnemyListLength,1 do
             if EnemyList[ei] ~= nil then
                 local e = EnemyList[ei]
-                e.SpriteIndex= 384+e.offset+e.timer%30//10*2
+                if EnemyList[ei].died==false then
+                    e.SpriteIndex= 384+e.offset+e.timer%30//10*2
+                else
+                    e.SpriteIndex= 384+8+e.timer%30//10*2
+                end
             end
         end
     end
@@ -209,6 +250,11 @@
         end
     end
     
+    function DrawText()
+        print((string.format("Life: %s",Player.life)),2,2,15)
+        print((string.format("Score: %s",Player.score)),100,2,15)
+    end
+
     function IncTimers()
         Player.timer = Player.timer+1
         for ei=0,EnemyListLength,1 do
@@ -230,8 +276,10 @@
         SetEnemySpriteIndex()
         
         cls(0)
+        map()
         DrawElements()
         
+        DrawText()
         IncTimers()
     end
 
@@ -242,7 +290,7 @@
                 loop=true	
         end
         if Player.timer%5==0 then
-            --cls(0)
+            cls(0)
             print("NINJA",70,30,14,true,3)
             print("press X to start",72,60,15)
             if btn(5) then
@@ -252,8 +300,10 @@
     end
 
     function End()
-        print("GAME OVER",70,30,14,true,3)
-        print("press X to quit",72,60,15)
+        cls(0)
+        print("GAME OVER",45,30,14,true,3)
+        print(string.format("score: %s",Player.score),70,60,15)
+        print("press X to quit",60,70,15)
         if btn(5) then
             exit()
         end
